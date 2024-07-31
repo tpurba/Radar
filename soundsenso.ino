4fa6f9f88e31f3aa7a052613ff943fa62ec826a5
tpurba <tpurba@byu.edu>
2024-07-30 22:56:41 -0600

    Finished the sweep and ping

--- soundsenso.ino
+++ soundsenso.ino
@@ -1,64 +1,56 @@
+//Servo test
 #include <ESP32Servo.h>
+#define echoPin 10
+#define trigPin 12
+#define servoPin 19
 
-Servo myServo;
-const int servoPin = 19;
+Servo myservo;
+
+long duration;
+int distance;
+
+int calculateDistance()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  return distance;
+}
 
 void setup() {
-  myServo.attach(servoPin);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  myservo.attach(servoPin);
+  myservo.write(0);
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  myServo.write(0);
-  delay(1000);
-  
-  myServo.write(-90);
-  delay(1000);
-  
-  myServo.write(-180);
-  delay(1000);
+  int i ;
+  for (i = 15; i <= 165; i++)
+  {
+    myservo.write(i);
+    delay(15);
+    calculateDistance();
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+  }
+  for (i = 165; i >= 15; i--)
+  {
+    myservo.write(i);
+    delay(15);
+    calculateDistance();
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+
+  }
 }
-
-
-
-
-//#include <ESP32Servo.h>
-//#include <Wire.h>
-//#define echoPin 4               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
-//#define trigPin 2               // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
-//Servo myServo;
-//const int servoPin = 18;
-//long duration, distance;
-//void setup(){
-//  Serial.begin (9600);
-//  pinMode(trigPin, OUTPUT);
-//  pinMode(echoPin, INPUT);
-//  myServo.attach(servoPin);
-//}
-//void loop(){
-//  // Move the servo to 0 degrees
-//  myServo.write(0);
-//  delay(1000); // Wait for 1 second
-//  
-//  // Move the servo to 90 degrees
-//  myServo.write(90);
-//  delay(1000); // Wait for 1 second
-//  
-//  // Move the servo to 180 degrees
-//  myServo.write(180);
-//  delay(1000); // Wait for 1 second
-//  
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(2);
-//  digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(10);
-//  digitalWrite(trigPin, LOW);
-//  
-//  duration = pulseIn(echoPin, HIGH);
-//  distance = duration / 58.2;
-//  String disp = String(distance);
-//
-//  Serial.print("Distance: ");
-//  Serial.print(disp);
-//  Serial.println(" cm");
-//  delay(1000);
-//}
